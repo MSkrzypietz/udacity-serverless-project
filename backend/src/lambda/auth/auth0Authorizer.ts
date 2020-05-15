86f34770998f0e11215427ce8e11de522fa69785
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-15 22:56:23 +0200

    remove unused import

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,7 +3,6 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
