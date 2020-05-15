d7a641ebf19a9291aeecf83e72b09b75f5f97930
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-15 23:43:21 +0200

    fix custom authorizer

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,6 +1,7 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
+import Axios from 'axios'
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import { Jwt } from '../../auth/Jwt'
@@ -8,11 +9,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-const jwksClient = require('jwks-rsa');
 const jwksUrl = 'https://dev-bfjkwot3.eu.auth0.com/.well-known/jwks.json'
-const client = jwksClient({
-  jwksUri: jwksUrl,
-});
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -57,10 +54,23 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
   const kid = jwt.header.kid
-  const signingKey = await client.getSigningKey(kid)
-  return verify(token, signingKey, { algorithms: ['RS256'] }) as JwtPayload
+  let cert
+
+  try {
+    const jwks = await Axios.get(jwksUrl)
+    const signingKey = jwks.data.keys.find(k => k.kid === kid)
+
+    if (!signingKey) {
+      throw new Error(`No signing key matching the kid '${kid}' was found`);
+    }
+
+    cert = `-----BEGIN CERTIFICATE-----\n${signingKey.x5c[0]}\n-----END CERTIFICATE-----`
+  } catch (e) {
+    logger.error('Failed to retrieve auth0 certificate', { error: e.message })
+  }
+  
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
