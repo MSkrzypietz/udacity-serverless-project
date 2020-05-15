ed4b4aee0678484bc93c0c1a776c50a09ae50db2
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-15 22:56:51 +0200

    implement todo retrieval

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,37 @@
 import 'source-map-support/register'
 
+import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todoTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  console.log('Processing event: ', event)
+
+  const userId = getUserId(event)
+
+  const result = await docClient.query({
+    TableName: todoTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': userId
+      },
+  }).promise()
+
+  const items = result.Items
+  items.forEach(item => delete item['userId'])
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
